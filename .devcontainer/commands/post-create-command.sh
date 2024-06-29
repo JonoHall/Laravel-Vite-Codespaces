bd31fdd41cb07f23b5dbd294f2bafd72fb672a14
Jono Hall <jonohall@gmail.com>
2024-06-29 13:12:30 +1200

    Create post-create-command.sh

---
+++ .devcontainer/commands/post-create-command.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+composer create-project laravel/laravel ${containerWorkspaceFolder}/tmp 
+shopt -s dotglob
+mv -f ${containerWorkspaceFolder}/tmp/* ${containerWorkspaceFolder}
+rm -d ${containerWorkspaceFolder}/tmp
+mv -f ${containerWorkspaceFolder}/devcontainer-postinstall.json ${containerWorkspaceFolder}/.devcontainer/devcontainer.json
+gh codespace rebuild -c ${devcontainerId}
