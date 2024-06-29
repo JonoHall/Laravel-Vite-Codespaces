098c95ad10af2ab88785896871636d398ce577ed
Jono Hall <jonohall@gmail.com>
2024-06-29 13:45:22 +1200

    Update post-create-command.sh

--- .devcontainer/commands/post-create-command.sh
+++ .devcontainer/commands/post-create-command.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -x
 composer create-project laravel/laravel $CONTAINER_WORKSPACE_FOLDER/tmp 
-shopt -s dotglob
-mv -f $CONTAINER_WORKSPACE_FOLDER/tmp/* $CONTAINER_WORKSPACE_FOLDER
+mv -f $CONTAINER_WORKSPACE_FOLDER/tmp/{.[!.],}* $CONTAINER_WORKSPACE_FOLDER
 rm -d $CONTAINER_WORKSPACE_FOLDER/tmp
 mv -f $CONTAINER_WORKSPACE_FOLDER/devcontainer-postinstall.json $CONTAINER_WORKSPACE_FOLDER/.devcontainer/devcontainer.json
