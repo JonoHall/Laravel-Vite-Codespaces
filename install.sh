b1a8cf82580cb505b71eb551754029feda69d103
Jono Hall <jonohall@gmail.com>
2024-06-30 20:06:54 +1200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 composer create-project laravel/laravel $CODESPACE_VSCODE_FOLDER/tmp
-mv -f $CODESPACE_VSCODE_FOLDER/tmp/{.,}* $CODESPACE_VSCODE_FOLDER
+shopt -s dotglob
+mv -f $CODESPACE_VSCODE_FOLDER/tmp/* $CODESPACE_VSCODE_FOLDER
 rm -rf $CODESPACE_VSCODE_FOLDER/tmp
 mv -f $CODESPACE_VSCODE_FOLDER/devcontainer-postinstall.json $CODESPACE_VSCODE_FOLDER/.devcontainer/devcontainer.json
 rm $CODESPACE_VSCODE_FOLDER/install.sh
