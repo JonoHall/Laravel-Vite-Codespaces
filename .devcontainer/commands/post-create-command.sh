7364d45b7355f628a0a65c0aae947652d324e06b
Jono Hall <jonohall@gmail.com>
2024-06-30 00:50:08 +0000

    update

--- .devcontainer/commands/post-create-command.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -x
-composer create-project laravel/laravel /workspaces/$RepositoryName/tmp
-mv -f /workspaces/$RepositoryName/tmp/{.,}* /workspaces/$RepositoryName
-rm -rf /workspaces/$RepositoryName/tmp
-mv -f /workspaces/$RepositoryName/devcontainer-postinstall.json /workspaces/$RepositoryName/.devcontainer/devcontainer.json
--- .devcontainer/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -x
-composer create-project laravel/laravel /workspaces/$CODESPACE_NAME/tmp
-mv -f /workspaces/$CODESPACE_NAME/tmp/{.,}* /workspaces/$CODESPACE_NAME
-rm -rf /workspaces/$CODESPACE_NAME/tmp
-mv -f /workspaces/$CODESPACE_NAME/devcontainer-postinstall.json /workspaces/$CODESPACE_NAME/.devcontainer/devcontainer.json
