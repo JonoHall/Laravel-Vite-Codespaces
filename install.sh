9321ac7d9b2583a5a3d36afe3ea3315d4bd49d38
Jono Hall <jonohall@gmail.com>
2024-06-30 16:35:42 +1200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ rm -rf $CODESPACE_VSCODE_FOLDER/tmp
 mv -f $CODESPACE_VSCODE_FOLDER/devcontainer-postinstall.json $CODESPACE_VSCODE_FOLDER/.devcontainer/devcontainer.json
 rm $CODESPACE_VSCODE_FOLDER/install.sh
 rm $CODESPACE_VSCODE_FOLDER/README.md
+gh codespace rebuild
