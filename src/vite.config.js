a6ac864ae0ceea71f5dd6d90e70d737894767b45
Jono Hall <jonohall@gmail.com>
2024-07-06 15:25:26 +1200

    Update and rename vite-config.js to vite.config.js

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,3 +1,13 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
     server: {
         hmr: {
             host: process.env.CODESPACE_NAME ? process.env.CODESPACE_NAME + '-5173.' + process.env.GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN : null,
@@ -5,3 +15,4 @@
             protocol: process.env.CODESPACE_NAME ? 'wss' : null
         },
     }
+});
