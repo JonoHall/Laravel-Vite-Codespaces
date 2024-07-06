22545a13d82ded618215538a9729db03ed9da6e8
Jono Hall <jonohall@gmail.com>
2024-07-06 15:47:35 +1200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ rm -rf $CODESPACE_VSCODE_FOLDER/tmp
 mv -f $CODESPACE_VSCODE_FOLDER/devcontainer-postinstall.json $CODESPACE_VSCODE_FOLDER/.devcontainer/devcontainer.json
 rm $CODESPACE_VSCODE_FOLDER/install.sh
 rm $CODESPACE_VSCODE_FOLDER/README.md
+cp ./src/vite.config.js ./vite.config.js
 sed -i '/->withMiddleware(function (Middleware $middleware) {/a \\t\t$middleware->trustProxies(at: "*");' ./bootstrap/app.php
 clear
 read -n 1 -s -r -p "Your Codespace has been configured and now needs rebuilding. This may take a moment. Press any key to continue"
