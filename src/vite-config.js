c4641d46198a45cb0d0e41adc5423a61c887ba9f
Jono Hall <jonohall@gmail.com>
2024-07-06 13:21:59 +1200

    Update vite-config.js

--- src/vite-config.js
+++ src/vite-config.js
@@ -1,6 +1,6 @@
     server: {
         hmr: {
-            host: process.env.CODESPACE_NAME ? process.env.CODESPACE_NAME + '-5173.app.github.dev' : null,
+            host: process.env.CODESPACE_NAME ? process.env.CODESPACE_NAME + '-5173.' + process.env.GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN : null,
             clientPort: process.env.CODESPACE_NAME ? 443 : null,
             protocol: process.env.CODESPACE_NAME ? 'wss' : null
         },
