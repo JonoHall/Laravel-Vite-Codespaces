85d0355f640189088d12d24e5d783ff476bc1b1e
Jono Hall <jonohall@gmail.com>
2024-07-06 13:16:25 +1200

    Create vite-config.js

---
+++ src/vite-config.js
@@ -0,0 +1,7 @@
+    server: {
+        hmr: {
+            host: process.env.CODESPACE_NAME ? process.env.CODESPACE_NAME + '-5173.app.github.dev' : null,
+            clientPort: process.env.CODESPACE_NAME ? 443 : null,
+            protocol: process.env.CODESPACE_NAME ? 'wss' : null
+        },
+    }
