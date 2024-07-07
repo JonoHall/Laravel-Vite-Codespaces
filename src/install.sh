5bce965df3ac61630189ce14452b0730d865cd34
Jono Hall <jonohall@gmail.com>
2024-07-07 20:41:30 +1200

    Update install.sh

--- src/install.sh
+++ src/install.sh
@@ -6,8 +6,8 @@ rm -rf $CODESPACE_VSCODE_FOLDER/tmp
 mv -f $CODESPACE_VSCODE_FOLDER/src/devcontainer-postinstall.json $CODESPACE_VSCODE_FOLDER/.devcontainer/devcontainer.json
 rm $CODESPACE_VSCODE_FOLDER/README.md
 cp ./src/vite.config.js ./vite.config.js
+sed -i "/withMiddleware/r ./src/trusted-proxy.php" ./bootstrap/app.php
 rm -r ./src
-sed -i '/->withMiddleware(function (Middleware $middleware) {/a \\tif (env("APP_ENV") == "local") {\n\t\t$middleware->trustProxies(at: "*");\n\t}' ./bootstrap/app.php
 clear
 echo "Your Codespace has been configured and now needs rebuilding. This may take a moment."
 gh codespace rebuild -c $CODESPACE_NAME
