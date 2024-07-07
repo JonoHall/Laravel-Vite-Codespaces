f259f6dbf0145b76834f326d3cc23cc807af8d79
Jono Hall <jonohall@gmail.com>
2024-07-07 14:04:13 +1200

    Update install.sh

--- src/install.sh
+++ src/install.sh
@@ -4,7 +4,6 @@ shopt -s dotglob
 mv -f $CODESPACE_VSCODE_FOLDER/tmp/* $CODESPACE_VSCODE_FOLDER
 rm -rf $CODESPACE_VSCODE_FOLDER/tmp
 mv -f $CODESPACE_VSCODE_FOLDER/src/devcontainer-postinstall.json $CODESPACE_VSCODE_FOLDER/.devcontainer/devcontainer.json
-rm $CODESPACE_VSCODE_FOLDER/install.sh
 rm $CODESPACE_VSCODE_FOLDER/README.md
 cp ./src/vite.config.js ./vite.config.js
 rm -r ./src
