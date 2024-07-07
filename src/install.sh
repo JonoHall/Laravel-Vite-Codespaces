a61418af42bc09b65b8532fad51bb53fecbe8a32
Jono Hall <jonohall@gmail.com>
2024-07-07 14:03:29 +1200

    Update install.sh

--- src/install.sh
+++ src/install.sh
@@ -3,7 +3,7 @@ composer create-project laravel/laravel $CODESPACE_VSCODE_FOLDER/tmp
 shopt -s dotglob
 mv -f $CODESPACE_VSCODE_FOLDER/tmp/* $CODESPACE_VSCODE_FOLDER
 rm -rf $CODESPACE_VSCODE_FOLDER/tmp
-mv -f $CODESPACE_VSCODE_FOLDER/devcontainer-postinstall.json $CODESPACE_VSCODE_FOLDER/.devcontainer/devcontainer.json
+mv -f $CODESPACE_VSCODE_FOLDER/src/devcontainer-postinstall.json $CODESPACE_VSCODE_FOLDER/.devcontainer/devcontainer.json
 rm $CODESPACE_VSCODE_FOLDER/install.sh
 rm $CODESPACE_VSCODE_FOLDER/README.md
 cp ./src/vite.config.js ./vite.config.js
