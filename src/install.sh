8b0c78d7920a592be0d8f6bbb0b407f0c619f4c4
Jono Hall <jonohall@gmail.com>
2024-07-08 19:43:32 +1200

    Update install.sh

--- src/install.sh
+++ src/install.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
-composer create-project laravel/laravel $CODESPACE_VSCODE_FOLDER/tmp
+
+#install Laravel to a temporary folder, as Laravel won't install into a non-empty directory
+composer create-project laravel/laravel=11.* $CODESPACE_VSCODE_FOLDER/tmp --prefer-dist
+
+#move all files including . files, then delete the now empty folder
 shopt -s dotglob
 mv -f $CODESPACE_VSCODE_FOLDER/tmp/* $CODESPACE_VSCODE_FOLDER
 rm -rf $CODESPACE_VSCODE_FOLDER/tmp
+
+
+#move the Codespace specific config files
 mv -f $CODESPACE_VSCODE_FOLDER/src/devcontainer-postinstall.json $CODESPACE_VSCODE_FOLDER/.devcontainer/devcontainer.json
-rm $CODESPACE_VSCODE_FOLDER/README.md
-cp ./src/vite.config.js ./vite.config.js
+cp ./src/vite-codespaces.config.js ./
+npm pkg set scripts.dev="vite --config vite-codespaces.config.js"
+
+#inject Trusted Proxy config into bootstrap config
 sed -i "/withMiddleware/r ./src/trusted-proxy.php" ./bootstrap/app.php
+
+#cleanup files
 rm -r ./src
+rm $CODESPACE_VSCODE_FOLDER/README.md
+
+#farewell and good luck!
 clear
 echo "Your Codespace has been configured and is rebuilding. Please refresh your browser to reconnect."
 gh codespace rebuild -c $CODESPACE_NAME
