69d09de75abb0377d906392231256683bf54f841
Jono Hall <jonohall@gmail.com>
2024-07-09 23:31:19 +1200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 #install Laravel to a temporary folder, as Laravel won't install into a non-empty directory
-composer create-project laravel/laravel=11.* $CODESPACE_VSCODE_FOLDER/tmp --prefer-dist
+composer create-project laravel/laravel=11.* ./tmp --prefer-dist
 
 #move all files including . files, then delete the now empty folder
 shopt -s dotglob
-rm $CODESPACE_VSCODE_FOLDER/README.md
-mv -f $CODESPACE_VSCODE_FOLDER/tmp/* $CODESPACE_VSCODE_FOLDER
-rm -rf $CODESPACE_VSCODE_FOLDER/tmp
+rm ./README.md
+mv -f ./tmp/* ./
+rm -rf ./tmp
 
 
 #move the Codespace specific config files
-mv -f $CODESPACE_VSCODE_FOLDER/src/devcontainer-postinstall.json $CODESPACE_VSCODE_FOLDER/.devcontainer/devcontainer.json
+mv -f ./src/devcontainer-postinstall.json ./.devcontainer/devcontainer.json
 mv ./src/vite-codespaces.config.js ./
 mv ./src/trustedproxy.php ./config
 sed -i '1s/^/TRUSTED_PROXIES=*\n /' .env
@@ -19,7 +19,7 @@ npm pkg set scripts.dev="vite --config vite-codespaces.config.js"
 
 #cleanup files
 rm -r ./src
-rm $CODESPACE_VSCODE_FOLDER/README.md
+rm ./README.md
 
 #farewell and good luck!
 clear
