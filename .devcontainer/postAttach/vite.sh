7b3e03ce95762fb0d75f660116b3c85b1db3e61b
Jono Hall <jonohall@gmail.com>
2025-01-25 09:08:26 +0000

    Clarified error message.

--- .devcontainer/postAttach/vite.sh
+++ .devcontainer/postAttach/vite.sh
@@ -4,5 +4,5 @@ if [ -e package.json ]
 then
     npm run dev
 else
-    echo "npm cannot start, package.json not found."
+    echo "npm cannot start, package.json not found. Laravel may not be installed."
 fi
